f354ba67be898c7104b4022f326061fa4f86466f
marvin espira <espiramarvin@gmail.com>
2022-08-31 10:37:05 +0300

    feat: add updates

--- api/server.js
+++ api/server.js
@@ -1,14 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logger } = require('./middleware/logger')
-const { errorHandler } = require('./middleware/errorHandler')
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions =  require('./config/corsOrigin')
+const connectDb = require('./config/dbConn')
+const mongoose = require('mongoose')
+
 const PORT = process.env.PORT ||4000
 
+console.log(process.env.NODE_ENV)
 
+connectDb()
 
 // save logs to file middleware
 app.use(logger)
@@ -42,4 +48,12 @@ app.all('*', (req, res) => {
 app.use(errorHandler) // error handler middleware
 console.log('eror handler middleware passed')
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+mongoose.connection.once('open',  () => {
+    console.log('connected to mongo db')
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+} )
