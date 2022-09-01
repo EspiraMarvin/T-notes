35d38e66341142d1901900ccd1bb4b0cd6d0ea93
marvin espira <espiramarvin@gmail.com>
2022-09-01 11:03:28 +0300

    feat: add updates

--- api/models/User.js
+++ api/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     roles: [{
         type: String,
-        default: "EMployee"
+        default: "Employee"
     }],
     active: {
         type: Boolean,
