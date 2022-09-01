675ff90d483ca7ed7704c62388ea5e22da29f887
marvin espira <espiramarvin@gmail.com>
2022-09-01 16:29:58 +0300

    chores: add front-end boilerplate

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="p-4 text-blue-500 text-2xl flex items-center justify-center">Tech Notes</h1>
+    </div>
+  );
+}
+
+export default App;
