2705387e238ad273b7eb09deeb1687be5df8a452
marvin espira <espiramarvin@gmail.com>
2022-09-03 15:06:59 +0300

    feat: adds data prefetch

---
+++ app/src/features/auth/Prefetch.js
@@ -0,0 +1,23 @@
+import { store } from '../../rtk/store'
+import { notesApiSlice } from '../notes/notesApiSlice'
+import { usersApiSlice } from '../users/usersApiSlice'
+import { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
+
+
+export default function Prefetch() {
+    useEffect(() => {
+        console.log('subscribing')
+        const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate())
+        const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
+
+
+        return () => {
+            console.log('unsubscribing')
+            notes.unsubscribe()
+            users.unsubscribe()
+        }
+    }, [])
+
+    return <Outlet />
+}
