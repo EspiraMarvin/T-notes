2594b3067a829fa03568f46b1f56eb21dde1ac9c
marvin espira <espiramarvin@gmail.com>
2022-09-15 16:44:58 +0300

    feat: add updates

--- app/src/features/users/usersApiSlice.js
+++ app/src/features/users/usersApiSlice.js
@@ -8,10 +8,12 @@ const initialState = usersAdapters.getInitialState()
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
         getUsers: builder.query({
-            query: () => '/users',
-            validateStatus: (response, result) => {
-                return response.status === 200 && !result.isError
-            },
+            query: () => ({
+                url:'/users',
+                validateStatus: (response, result) => {
+                    return response.status === 200 && !result.isError
+                },
+            }),
             transformResponse: responseData => {
                 const loadedUsers = responseData.map(user => {
                     console.log('user at userApiSlice', user)
