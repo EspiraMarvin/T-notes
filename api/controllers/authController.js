e141a88985fe330fe6a3a3a36066daf4572cf66c
marvin espira <espiramarvin@gmail.com>
2022-09-28 12:16:48 +0300

    feat: add updates

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-// const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler')
 
 
 // @desc Login
